9e0a77c0851ab3f5c366326afaaf26b69398b511
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2025-04-03 15:08:18 -0300

    fix: fixed seeder error

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Travel;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
  */
@@ -17,9 +18,9 @@ class TicketFactory extends Factory
     public function definition(): array
     {
         return [
-            'code' => $this->faker->numberBetween(1000,9999),
-            'seat' => $this->faker->numberBetween(30,60),
-            'total' => $this->faker->numberBetween(100,1000),
+            'code' => $this->faker->unique()->numberBetween(1000, 9999), // Asegura unicidad
+            'seat' => $this->faker->numberBetween(30, 60),
+            'total' => $this->faker->numberBetween(100, 1000),
             'date' => $this->faker->date('Y-m-d'),
             'travel_id' => Travel::factory(),
         ];
